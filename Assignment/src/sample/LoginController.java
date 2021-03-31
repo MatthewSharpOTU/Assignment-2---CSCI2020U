3b838e5a1c874b78a9e452142bfa56ec0498efaf
Andy Wang <wei.wang8@ontariotechu.net>
2021-03-30 22:12:10 -0400

    Added directory selection

--- Assignment/src/sample/LoginController.java
+++ Assignment/src/sample/LoginController.java
@@ -4,23 +4,52 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.File;
 import java.io.IOException;
 
 public class LoginController {
     @FXML TextField textField;
+    @FXML Label clientDirDisplay;
 
+    private File clientFile;
     public void LoginWithUsername() throws IOException {
-        //this method is called when the login login button is pressed
+        //checking fields before continuing
+        String username = textField.getText();
+        if (username.length()==0){
+            textField.setPromptText("Requires Username");
+            return;
+        }
+        textField.clear();
+        if (clientFile!=null && clientFile.exists() && clientFile.isFile()){
+            clientDirDisplay.setText("Error");
+            return;
+        }
 
         //opens up the main client ui window and closes this login window
         Stage mainStage = (Stage) textField.getScene().getWindow();
-        Parent animationRoot = FXMLLoader.load(getClass().getResource("clientUi.fxml"));
-        Scene currentMainScene = new Scene(animationRoot);
-        mainStage.setScene(currentMainScene);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("clientUi.fxml"));
+        mainStage.setScene(loader.load());
 
+        //passes client directory to table view
+        MainClientUiController controller = loader.getController();
+        controller.initData(clientFile);
+        
         //anything like setting up a list of files should be done in the MainClientUiController.initialize()
+    }
+
+    public void setClientDirectory(){
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        clientFile = directoryChooser.showDialog(textField.getScene().getWindow());
+        if (clientFile.isDirectory()){
+            clientDirDisplay.setText(clientFile.getName());
+        }else{
+            clientDirDisplay.setText("None");
+        }
     }
 }
--- Assignment/src/sample/MainClientUiController.java
+++ Assignment/src/sample/MainClientUiController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
+import java.io.File;
+
 //this is the controller class that is connected to the main ui with the upload and download
 public class MainClientUiController {
     @FXML Button uploadButton;
@@ -11,8 +13,14 @@ public class MainClientUiController {
     @FXML ListView<String> clientListView; //you can change the type from String to anything you need
     @FXML ListView<String> serverListView;
 
+    File clientFile;
     public void initialize(){
 
+    }
+
+    //method used for passing client file dir
+    public void initData(File clientFile){
+        this.clientFile = clientFile;
     }
 
     //methods are set up to be called when ever the buttons are pressed
